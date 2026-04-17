27b6e4b17a8efc0a26a26565cd932aad356b21ac
Christoph Peter-Silie <christoph@MacBook-Pro-von-Christoph.local>
2026-04-17 07:59:40 +0300

    v2.3.0 alles auf ESM projekt supabase unified

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://xdchyruasjxvrjduchoc.supabase.co'
+const SUPABASE_URL = 'https://yjipgfddyoiwylernuje.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_6gFWXVtCVf7mdtKw6ltYtw_AMI4fGOr'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabaseSnapshots = supabase
