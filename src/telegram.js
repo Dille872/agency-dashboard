5888138fcd50754f8d45438b4f07dc8ed7f67236
Christoph Peter-Silie <christoph@MacBook-Pro-von-Christoph.local>
2026-04-28 09:18:58 +0300

    rotate bot token

--- src/telegram.js
+++ src/telegram.js
@@ -1,4 +1,4 @@
-const BOT_TOKEN = '8396910457:AAE4nH89-FbVjeFLIrHRJoNMUO1EFZs_aIA
+const BOT_TOKEN = '8396910457:AAEVuNIlRGlNfa0_vUaONh6QGt7VCYpDAyE'
 const OWNER_ID = '1538601588'
 const API = `https://api.telegram.org/bot${BOT_TOKEN}`
 
